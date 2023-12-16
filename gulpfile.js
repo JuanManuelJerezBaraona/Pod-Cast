e6f9404bfdddf4f8a4c9b65a7f790af270fdcbbe
Juan Manuel Jerez <jjerezbaraona@gmail.com>
2023-12-16 13:14:31 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { } = require('gulp');
+
+function css(done) {
+    // Identificar el archivo principal
+
+
+    // Compilar SASS
+
+
+    // Exportarlo o guardarlo en una ubicación
+    done();
+}
+
+exports.css = css;
